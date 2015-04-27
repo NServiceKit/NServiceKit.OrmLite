3184054a28c05eef6198d27a73e2f04505452538
Rajah James <intelblaster@gmail.com>
2015-04-27 06:58:35 -0400

    Fix build.sh for Travis.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 export EnableNuGetPackageRestore=true
-xbuild ./src/NServiceKit.Text.sln /p:TargetFramework=net40 /p:Configuration=Release
+xbuild ./src/NServiceKit.OrmLite.sln /p:TargetFramework=net40 /p:Configuration=Release
